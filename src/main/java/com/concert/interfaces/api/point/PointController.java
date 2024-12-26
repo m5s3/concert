5913256e428d0f3627fdd5b1000c0d4678627103
m5s3 <devmyoungsoo2@gmail.com>
2024-12-26 18:46:06 +0900

    refactor(Point): 디버깅 코드 삭제

--- src/main/java/com/concert/interfaces/api/point/PointController.java
+++ src/main/java/com/concert/interfaces/api/point/PointController.java
@@ -5,14 +5,12 @@ import com.concert.interfaces.api.common.ApiResponse;
 import com.concert.interfaces.api.point.dto.PointResponse;
 import com.concert.interfaces.api.point.dto.UpdatePointRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/points")
@@ -23,10 +21,8 @@ public class PointController {
     @PostMapping("/charge")
     public ResponseEntity<ApiResponse<PointResponse>> charge(@RequestBody UpdatePointRequest request) {
         try {
-            log.info("charge request: {}", request);
             return ResponseEntity.ok(ApiResponse.success(PointResponse.from(pointFacade.charge(request.toCommand()))));
         } catch (Exception e) {
-            e.printStackTrace();
             return ResponseEntity.ok(ApiResponse.success(null));
         }
     }
