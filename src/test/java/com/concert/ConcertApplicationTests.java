d7dd554024757fc174feb15617be02a64dae3a7a
m5s3 <devmyoungsoo2@gmail.com>
2025-01-17 13:47:19 +0900

    docs(concnert): rest docs create conert api 수정

--- src/test/java/com/concert/ConcertApplicationTests.java
+++ src/test/java/com/concert/ConcertApplicationTests.java
@@ -1,9 +1,12 @@
 package com.concert;
 
+import com.concert.config.TestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestContainerConfig.class)
 class ConcertApplicationTests {
 
     @Test
